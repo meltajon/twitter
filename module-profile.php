12edca3525644e608cdfc4301f5839e427d6be66
Mel Tajon <meltajon@gmail.com>
2016-10-09 22:23:58 -0700

    fixed module-profile for mobile

--- module-profile.php
+++ module-profile.php
@@ -4,9 +4,11 @@
 		<div id="profile">
 			<div class="panel1-wrapper">
 				<img id="profile-avatar" src="uploads/avatar-mel-tajon.jpg" />
+			</div><!-- panel1-wrapper -->
+			<div class="panel2-wrapper">
 				<h1 id="profile-display-name">Mel Tajon</h1>
 				<div id="profile-description">Front-End Web Developer • User Experience • Foodie • Dog Lover</div>
 				<div id="profile-url"><a href="#">hire.meltajon.com</a></div>
 				<button id="profile-follow-button" href="#">Follow<ins> @MelTajon</ins></button>
-			</div><!-- panel1-wrapper -->
+			</div><!-- panel2-wrapper -->
 		</div><!-- profile -->
