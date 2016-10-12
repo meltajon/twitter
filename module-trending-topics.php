c60a7d87e72eb53564988299dfddf13140d9a232
Mel Tajon <meltajon@gmail.com>
2016-10-11 23:18:24 -0700

    updated #panel3 for mobile

--- module-trending-topics.php
+++ module-trending-topics.php
@@ -5,16 +5,16 @@
 				<div class="aside-wrapper">
 					<h2 id="trending-topics-title">Trending</h2>
 					<ul id="trending-topics-list">
-						<li><a href="#">#USAvsBZL</a></li>
-						<li><a href="#">#TrumpSucks</a></li>
-						<li><a href="#">#TrumpSucks</a></li>
-						<li><a href="#">#TrumpSucks</a></li>
-						<li><a href="#">#TrumpSucks</a></li>
-						<li><a href="#">#TrumpSucks</a></li>
-						<li><a href="#">#TrumpSucks</a></li>
-						<li><a href="#">#TrumpSucks</a></li>
-						<li><a href="#">#TrumpSucks</a></li>
-						<li><a href="#">#TrumpSucks</a></li>
+						<li><a href="#">#ThisIsUs</a></li>
+						<li><a href="#">#AgentsOfShield</a></li>
+						<li><a href="#">#RiseFestival</a></li>
+						<li><a href="#">#Note7Explosions</a></li>
+						<li><a href="#">#ImWithHer</a></li>
+						<li><a href="#">#TrumpTapes</a></li>
+						<li><a href="#">#GuyFieriBooks</a></li>
+						<li><a href="#">Los Angeles Lakers</a></li>
+						<li><a href="#">D'Angelo Russell</a></li>
+						<li><a href="#">Tim Tebow</a></li>
 					</ul>
 				</div><!-- aside-wrapper -->
 			</aside>
